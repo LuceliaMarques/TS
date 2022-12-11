6dd31353eec081bd12bc62f77c3df4ac65906ef2
Lucélia Marques <luceliatmf@gmail.com>
2022-12-11 15:00:20 -0300

    atividade 02

--- at-01/src/index.ts
+++ at-01/src/index.ts
@@ -1,10 +1,40 @@
-class Validator
-{
-    data : number | string | boolean | undefined | null | void;
-    constructor(data : number | string | boolean | undefined | null | void){
-        this.data = data;
+class Validator {
+    _data: any;
+    constructor(data: any) {
+      this._data = data;
     }
-}
-
-let valid = new Validator("data");
-console.log(valid.data);
+  }
+  
+  class StringValidator extends Validator {
+    constructor(data: any) {
+      if (typeof data === "string") {
+        super(data);
+      } else {
+        throw new Error("O tipo está errado");
+      }
+    }
+  }
+  
+  class NumberValidator extends Validator {
+    constructor(data: any) {
+      if (typeof data === "number") {
+        super(data);
+      } else {
+        throw new Error("O tipo está errado");
+      }
+    }
+  }
+  
+  class BooleanValidator extends Validator {
+    constructor(data: any) {
+      if (typeof data === "boolean") {
+        super(data);
+      } else {
+        throw new Error("O tipo está errado");
+      }
+    }
+  }
+  
+  const obj = new BooleanValidator(5);
+  console.log("teste: ", obj._data);
+  
